83a149bbeed8f8416c1270622454fa2ca3190c0c
ThuhtetCoder <59603198+ThuhtetCoder@users.noreply.github.com>
2020-03-14 10:45:59 +0630

    change appname

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rentook
                 </div>
 
                 <div class="links">
